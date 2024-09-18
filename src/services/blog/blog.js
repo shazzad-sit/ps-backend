539b5bc571d50776c8025f3ffe73cce1a0ad97ec
Shazzad-Hossen <tipsfire.com@gmail.com>
2024-09-19 00:24:03 +0600

    blog details protection removed

--- src/services/blog/blog.js
+++ src/services/blog/blog.js
@@ -5,7 +5,7 @@ const { create, getAll, deleteOne, editOne, getSingle } = require("./blog.entity
 function blog (){
     this.route.post('/blog', auth, checkRole(['admin']), create(this));
     this.route.get('/blog', getAll(this));
-    this.route.get('/blog/:id', auth, getSingle(this));
+    this.route.get('/blog/:id', getSingle(this));
     this.route.delete('/blog/:id', auth, checkRole(['admin']), deleteOne(this));
     this.route.patch('/blog/', auth, checkRole(['admin']), editOne(this));
 
